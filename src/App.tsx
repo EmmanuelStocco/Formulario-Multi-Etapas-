04fe4f4c0750f98348398f63c7e61cd6faa0ed73
EmmanuelStocco <56724388+EmmanuelStocco@users.noreply.github.com>
2022-01-23 17:10:01 -0300

    context configurado e rotas iniciais criadas

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Router } from "./router"
+import { FormProvider } from'./contexts/FormContext';
+
+export default function App() { 
+  return(
+    <FormProvider>
+       <Router />
+     </FormProvider>
+  )
+}
