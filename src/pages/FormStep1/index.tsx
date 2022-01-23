da5cb5dc9f6aff87ae5bcc721ee64187274f85ea
EmmanuelStocco <56724388+EmmanuelStocco@users.noreply.github.com>
2022-01-23 17:25:13 -0300

    criação e utilização do theme global

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,9 +1,24 @@
 import * as C from './styles';
+import { Theme } from '../../components/theme';
 
 export const FormStep1 = () => {
     return(
-        <C.Container>
-            Passo1 
-        </C.Container>
+        <Theme>
+            <C.Container>
+                <p> Passo 1/3 </p>
+                <h1>Vamos começar com seu nome </h1> 
+                <p>Preencha o campo abaixo com seu nome completo </p>
+
+                <hr />
+
+                <label>
+                    Seu nome Completo
+                    <input 
+                        type="text"
+                        autoFocus    
+                     />
+                </label>
+            </C.Container>
+        </Theme>
     )
 }
