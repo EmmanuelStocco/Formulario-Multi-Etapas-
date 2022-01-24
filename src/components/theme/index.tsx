bba450072b87d4f67a79f8e899cc2080133c3b73
EmmanuelStocco <56724388+EmmanuelStocco@users.noreply.github.com>
2022-01-24 11:37:27 -0300

    passo 3 finalizado - enviando dados

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -1,12 +1,16 @@
 import { ReactNode } from 'react';
 import * as C from './styles';
 import { Header } from '../Header'
+import {useForm} from '../../contexts/FormContext'
+import { SidebarItem } from '../SidebarItem'; 
 
 type Props = {
     children: ReactNode;
 }
 
 export const Theme = ({ children }: Props) => {
+    const {state} = useForm();
+
     return (
         <C.Container>
             <C.Area>
@@ -14,9 +18,35 @@ export const Theme = ({ children }: Props) => {
 
                 <C.Steps>
                     <C.Sidebar>
-                        Sidebar
-                    </C.Sidebar>
+                        
+                        <SidebarItem
+                            title="Pessoal"
+                            description="se identigfique"
+                            icon="profile"
+                            path="/"
+                            active={state.currentStep === 1}
+                        />
 
+                        <SidebarItem
+                            title="profiosion"
+                            description="se identigfique"
+                            icon="book"
+                            path="/step2"
+                            active={state.currentStep === 2}
+
+                        />
+
+                        <SidebarItem
+                            title="Contatos"
+                            description="Como te achar"
+                            icon="mail"
+                            path="/step3"
+                            active={state.currentStep === 3}
+
+                        />
+                        
+
+                    </C.Sidebar>
                     <C.Page>
                         {children}
                     </C.Page>    
