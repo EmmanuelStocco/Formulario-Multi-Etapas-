08584096edc78dad32915c532a2789d9e6dc7676
EmmanuelStocco <56724388+EmmanuelStocco@users.noreply.github.com>
2022-01-24 11:08:10 -0300

    paginação finalizada

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,11 +1,39 @@
 import * as C from './styles';
 import { Theme } from '../../components/theme';
+import { useHistory } from 'react-router-dom';
+import {useForm, FormActions} from '../../contexts/FormContext';
+import { ChangeEvent, useEffect } from 'react';
 
 export const FormStep1 = () => {
+    const history = useHistory();
+    const { state, dispatch } = useForm();
+
+    useEffect(()=> {
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+        });
+    }, []);
+
+    const handleNextStep = () => {
+        if(state.name !== ''){
+            history.push('/step2');
+        } else {
+            alert('Preencha os dados! ')
+        }
+    };
+
+    const handleNameChange = (e :ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        })
+    }
+
     return(
         <Theme>
             <C.Container>
-                <p> Passo 1/3 </p>
+                <p>Passo 1/3  </p> 
                 <h1>Vamos começar com seu nome </h1> 
                 <p>Preencha o campo abaixo com seu nome completo </p>
 
@@ -16,9 +44,15 @@ export const FormStep1 = () => {
                     <input 
                         type="text"
                         autoFocus    
+                        value={state.name}
+                        onChange={handleNameChange}
                      />
                 </label>
+
+                <button onClick={handleNextStep}> Próximo </button>
+
+
             </C.Container>
         </Theme>
     )
-}
+};
--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,9 +1,73 @@
 import * as C from './styles';
+import { Theme } from '../../components/theme';
+import { useHistory } from 'react-router-dom';
+import {useForm, FormActions} from '../../contexts/FormContext';
+import { ChangeEvent, useEffect } from 'react';
+import { SelectOption } from '../../components/SelectOption/index';
+import { Link } from 'react-router-dom';
 
 export const FormStep2 = () => {
+    const history = useHistory();
+    const { state, dispatch } = useForm();
+
+    useEffect(()=> {
+        if(state.name ===''){
+            history.push('/');
+        } else{
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 2 
+        });
+    }
+    }, []);
+  
+    function setLevel (level: number){
+        dispatch({
+            type: FormActions.setLevel,
+            payload: level
+        })
+    }
+
+    const handleNextStep = () => {
+        if(state.name !== ''){
+            history.push('/step3');
+        } else {
+            alert('Preencha os dados! ')
+        }
+    };
+
     return(
-        <C.Container>
-            Passo2 
-        </C.Container>
+        <Theme>
+            <C.Container>
+                <p>Passo 2/3  </p> 
+                <h1>{state.name} o que melhor descreve você </h1> 
+                <p>Preencha o campo abaixo com a opção profissional que mais te representa </p>
+
+                <hr />
+
+                <SelectOption 
+                    title="Sou iniciante"
+                    description="Comecei a programar a menos de 2 anos"
+                    icon="O"
+                    selected={state.level === 0}
+                    onClick={() => setLevel(0) }
+                />
+
+                <SelectOption 
+                    title="Sou programador"
+                    description="Já programo a mais de 2 anos"
+                    icon="X"
+                    selected={state.level === 1}
+                    onClick={() => setLevel(1) }
+
+                />
+ 
+
+                <Link to="/" className='backButton'> Voltar </Link>
+                <button onClick={handleNextStep}> Próximo </button>
+
+
+            </C.Container>
+        </Theme>
     )
-}
+};
